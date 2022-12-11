c4889f28564bf65373a9c112c2e332daed9c5c36
makifyigitt <mehmetakif.yighit@gmail.com>
2022-12-11 17:15:51 +0300

    deneme

---
+++ src/test/java/com/may/kodluyoruzspringboot/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.kodluyoruzspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
